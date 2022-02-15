39395d488a78065dc33f6850007f0d86131365fd
Adam Biedka Whitton <adambb914@gmail.com>
2022-02-15 16:57:06 +0100

    ongoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do |i|
+  Article.create(title: "#{Faker::Verb.ing_form} #{Faker::Food.spice}", content: "#{Faker::Food.ingredient} #{Faker::Verb.ing_form} #{Faker::Food.dish}")
+end
